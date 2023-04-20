3fe0dacf818cb81647613dbb474ba5566c97556a
ByCooper <kup.official@yahoo.com>
2023-04-20 14:47:49 +0300

    Курсовая работа (повышенная сложность)

---
+++ src/Employee.java
@@ -0,0 +1,118 @@
+import java.text.DecimalFormat;
+import java.util.Arrays;
+
+public class Employee {
+    String firstName;
+    String middleName;
+    String lastName;
+    int office;
+    int salary;
+    int id;
+    public static int count;
+//Для задания повышенной сложности ================================================================================
+    static int min;
+    static Employee minU;
+    static int max;
+    static Employee maxU;
+
+
+
+    public Employee(String fName, String mName, String lName, int office, int salary) {
+        this.firstName = fName;
+        this.middleName = mName;
+        this.lastName = lName;
+        this.office = office;
+        this.salary = salary;
+        id = ++count;
+
+    }
+
+    @Override
+    public String toString() {
+        return '\n' + "Сотрудник" + '\n' + "id = " + id + '\n' +
+                "Фамилия ='" + this.middleName + '\'' + '\n' +
+                "Имя ='" + this.firstName + '\'' + '\n' +
+                "Отчество ='" + this.lastName + '\'' + '\n' +
+                "Отдел = " + this.office + '\n' +
+                "Зарплата = " + this.salary + '\n';
+    }
+
+    public void setOffice(int office) {
+        this.office = office;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getOffice() {
+        return office;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public static int allSalary(Employee[] o) {
+        int sum = 0;
+        int a = 0;
+        for (int i = 0; i <= o.length - 1; i++) {
+            a = o[i].salary;
+            sum = sum + a;
+        }
+        return sum;
+    }
+
+    public static void minSalary(Employee[] o) {
+        int min = o[0].salary;
+        Employee unitMin = o[0];
+        for (int i = 0; i <= o.length - 1; i++) {
+            if (min > o[i].salary) {
+                min = o[i].salary;
+                unitMin = o[i];
+            }
+        }
+        System.out.println("Минимальная зарплата по организации: " + "\n" + unitMin);
+    }
+
+    public static void maxSalary(Employee[] o) {
+        int max = o[0].salary;
+        Employee unitMax = o[0];
+        for (int i = 0; i <= o.length - 1; i++) {
+            if (max < o[i].salary) {
+                max = o[i].salary;
+                unitMax = o[i];
+            }
+        }
+        System.out.println("Максимальная зарплата по организации: " + "\n" + unitMax);
+    }
+
+    public static void averageSalary(Employee[] o, int i) {
+        double average = (double) i / o.length;
+        System.out.println("Среднее занчение зарплат равно " + new DecimalFormat("###,###.##").format(average));
+    }
+
+    public static void allName(Employee[] o) {
+        for (int i = 0; i <= o.length - 1; i++) {
+            System.out.println(o[i].middleName + " " + o[i].firstName + " " + o[i].lastName);
+        }
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee[] employees = new Employee[10];
+        Employee employee1 = new Employee("Иван", "Иванов", "Сергеевич", 2, 19000);
+        Employee employee2 = new Employee("Егор", "Петров", "Ильич", 1, 18000);
+        Employee employee3 = new Employee("Илья", "Серов", "Александрович", 5, 13000);
+        Employee employee4 = new Employee("Игнат", "Нилов", "Иванович", 3, 16000);
+        Employee employee5 = new Employee("Игорь", "Валов", "Николаевич", 4, 20000);
+        Employee employee6 = new Employee("Сергей", "Шинов", "Андреевич", 2, 15500);
+        Employee employee7 = new Employee("Дмитрий", "Сергеев", "Олегович", 4, 19000);
+        Employee employee8 = new Employee("Николай", "Цой", "Артемович", 5, 14000);
+        Employee employee9 = new Employee("Тимофей", "Тян", "Дмитриевич", 3, 15200);
+        Employee employee10 = new Employee("Владимир", "Вашин", "Сергеевич", 2, 18000);
+
+        employees[0] = employee1;
+        employees[1] = employee2;
+        employees[2] = employee3;
+        employees[3] = employee4;
+        employees[4] = employee5;
+        employees[5] = employee6;
+        employees[6] = employee7;
+        employees[7] = employee8;
+        employees[8] = employee9;
+        employees[9] = employee10;
+
+        System.out.println(Arrays.toString(employees));
+        System.out.println();
+        System.out.println("Ежемесячные расходы на зарплаты для сотрудников составляют " + Employee.allSalary(employees));
+        System.out.println();
+        Employee.minSalary(employees);
+        System.out.println();
+        Employee.maxSalary(employees);
+        System.out.println();
+        Employee.averageSalary(employees, Employee.allSalary(employees));
+        System.out.println();
+        Employee.allName(employees);
+        System.out.println();
+
+
+    }
+}
